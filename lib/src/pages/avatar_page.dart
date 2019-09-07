4a9769add9e8010537e4897f170e6d5587fd29a7
eniliosarcos <eniliosarcos1993@gmail.com>
2019-09-07 12:06:08 -0400

    class 77 widget circle avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -11,6 +11,37 @@ class AvatarPage extends StatelessWidget
       appBar: AppBar
       (
         title: Text('Avatar Page'),
+        actions: <Widget>
+        [
+          Container
+          (
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar
+            (
+              backgroundImage: NetworkImage('https://pbs.twimg.com/profile_images/892018248757239808/byFBZNEN_400x400.jpg'),
+              radius: 23.0,
+            ),
+          ),
+
+          Container
+          (
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar
+            (
+              child: Text('EJSP'),
+              backgroundColor: Colors.purple,
+            ),
+          )
+        ],
+      ),
+      body: Center
+      (
+        child: FadeInImage
+        (
+          image: NetworkImage('https://pbs.twimg.com/profile_images/378800000061284068/f4680d46b1493b5820aaec1ae9a94707_400x400.jpeg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),      
     );
   }
