e923885e38af77df0a9d83839f5900db79f911ce
eniliosarcos <eniliosarcos1993@gmail.com>
2019-09-12 12:13:06 -0400

    class 84 sliders

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -6,6 +6,7 @@ import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/inputs_page.dart';
+import 'package:componentes/src/pages/slider_page.dart';
 import 'package:flutter/material.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes()
@@ -18,5 +19,6 @@ Map<String, WidgetBuilder> getApplicationRoutes()
       'card' : (BuildContext context) => CardPage(),
       'animatedContainer' : (BuildContext context) => AnimatedContainerPage(),  
       'inputs' : (BuildContext context) => InputsPage(),
+      'slider' : (BuildContext context) => SliderPage(),
     };
 }
